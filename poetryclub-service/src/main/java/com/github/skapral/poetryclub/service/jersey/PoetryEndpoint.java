2d50da2da7f463857b95b175dceaced8e4e3fffd
Kapralov Sergey <skapralov@mail.ru>
2018-09-21 17:58:38 +0300

    [#15] Checkstyle fixes

--- poetryclub-service/src/main/java/com/github/skapral/poetryclub/service/jersey/PoetryEndpoint.java
+++ poetryclub-service/src/main/java/com/github/skapral/poetryclub/service/jersey/PoetryEndpoint.java
@@ -1,3 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Kapralov Sergey
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
+
 package com.github.skapral.poetryclub.service.jersey;
 
 import com.github.skapral.poetryclub.core.scalar.ScalarStatic;
@@ -40,9 +66,15 @@ public class PoetryEndpoint {
         ).content();
     }
 
+    /**
+     * Summary page
+     * @param req Request
+     * @param communityId Community ID
+     * @return Response
+     */
     @GET
     @Path("{communityId}/summary.html")
-    public String index(@Context HttpServletRequest req, @PathParam("communityId") String communityId) {
+    public String summary(@Context HttpServletRequest req, @PathParam("communityId") String communityId) {
         return new TmplSummary(
             new ScalarUUID(communityId)
         ).content();
